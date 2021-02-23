183835051b7c8c8a1d140465fb97d6eca8a17596
Kyle Ohlwiler <kohlwiler@yahoo.com>
2021-02-22 18:13:24 -0800

    added code for where API query will go

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ document.getElementById('search').addEventListener('click', event => {
           let location = res.data
           console.log(location)
 
+          document.getElementById('currentCity').innerHTML = `
+            <h2></h2>
+            <p></p>
+            <p></P>
+            <p></P>
+            <p></P>
+          `
+          document.getElementById('forecast').innerHTML = `
+          <h2>5-Day Forecast:
+          
+          `
+
 
          document.getElementById('search').value = '' 
       })
