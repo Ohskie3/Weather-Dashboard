6d96246dc6600c8e22190ee38c8c5cf7affe69cd
Kyle Ohlwiler <kohlwiler@yahoo.com>
2021-02-23 17:21:19 -0800

    added five day forecase

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
-document.getElementById('search').addEventListener('click', event => {
+document.getElementById('searchBtn').addEventListener('click', event => {
     event.preventDefault()
     
     let name = document.getElementById('search').value
 
-  axios.get(`put URL here`)
+  // a0facbfa14a51a09cfa5af1f8db212b0
+  axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${name}&units=imperial&appid=a0facbfa14a51a09cfa5af1f8db212b0`)
       .then(res => {
           let location = res.data
           console.log(location)
 
           document.getElementById('currentCity').innerHTML = `
-            <h2></h2>
-            <p></p>
-            <p></P>
-            <p></P>
-            <p></P>
+            <h2>${location.city.name} (2/23/2021)</h2>
+            <p>Temperature: ${location.list[0].main.temp} °F</p>
+            <p>Humidity: ${location.list[0].main.humidity}%</P>
+            <p>Wind speed: ${location.list[0].wind.speed} MPH</P>
+            <p>UV index: </P>
+            <hr>
           `
           document.getElementById('forecast').innerHTML = `
-          <h2>5-Day Forecast:
-          
+          <h2>5-Day Forecast:</h2>
+          <div id="currentForecast">
+          <p>3/24/2021</p>
+          <p>Temp: ${location.list[3].main.temp} °F</p>
+          <p>Humidity: ${location.list[3].main.humidity}%<p/>
+          </div>
+          <div id="currentForecast">
+          <p>3/25/2021</p>
+          <p>Temp: ${location.list[11].main.temp} °F</p>
+          <p>Humidity: ${location.list[11].main.humidity}%<p/>
+          </div>
+          <div id="currentForecast">
+          <p>3/26/2021</p>
+          <p>Temp: ${location.list[19].main.temp} °F</p>
+          <p>Humidity: ${location.list[19].main.humidity}%<p/>
+          </div>
+          <div id="currentForecast">
+          <p>3/27/2021</p>
+          <p>Temp: ${location.list[27].main.temp} °F</p>
+          <p>Humidity: ${location.list[27].main.humidity}%<p/>
+          </div>
+          <div id="currentForecast">
+          <p>3/28/2021</p>
+          <p>Temp: ${location.list[35].main.temp} °F</p>
+          <p>Humidity: ${location.list[35].main.humidity}%<p/>
+          </div>
           `
 
 
